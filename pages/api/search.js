82860875d060478df4f2ab48e9905f3b21ffd136
a <a@A>
2020-01-06 23:07:18 +0800

    post

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,23 +5,19 @@ export default async (req, res) => {
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     'Content-Type': 'application/json'
   })
-  if (req.method === 'POST' && req.body) {
-    const url = 'https://www.google.com'
-    request.get({
-      url: url,
-      headers: {
-        // 'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-      },
-    }, (error, r, body) => {
-      if (!error) {
-        // res.send(body)
-        res.end(body)
-      } else {
-        res.json({ error })
-      }
-    })
-  } else {
-    res.end()
-  }
+  const url = 'https://www.google.com'
+  request.get({
+    url: url,
+    headers: {
+      // 'Accept': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+    },
+  }, (error, r, body) => {
+    if (!error) {
+      // res.send(body)
+      res.end(body)
+    } else {
+      res.json({ error })
+    }
+  })
 }
